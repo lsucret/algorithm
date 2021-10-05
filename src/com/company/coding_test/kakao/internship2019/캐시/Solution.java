23d94caabe83885396106f2183cdab0a796a09e9
lsucret <willow_weed@naver.com>
2021-10-05 23:56:45 +0900

    coding test solved(ing)

---
+++ src/com/company/coding_test/kakao/internship2019/캐시/Solution.java
@@ -0,0 +1,30 @@
+package com.company.coding_test.kakao.internship2019.캐시;
+
+import java.util.Stack;
+
+/**
+ * 코딩테스트 연습
+ * 2018 KAKAO BLIND RECRUITMENT
+ * [1차] 캐시
+ * https://programmers.co.kr/learn/courses/30/lessons/17680?language=java#_=
+ *
+ */
+class Solution {
+
+    public static void main(String[] args) {
+        int cacheSize = 5;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
+        Solution s = new Solution();
+        s.solution(cacheSize, cities);
+    }
+
+    /**
+     *
+    */
+    public int solution(int cacheSize, String[] cities) {
+
+
+        int answer = 0;
+        return answer;
+    }
+}
