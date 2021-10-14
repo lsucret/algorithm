7e8840090dbdce35a16ead26e9de9463795549a2
lsucret <willow_weed@naver.com>
2021-10-15 01:08:55 +0900

    coding test ing

---
+++ src/com/company/coding_test/kakao/internship2019/자물쇠와열쇠/Solution.java
@@ -0,0 +1,21 @@
+package com.company.coding_test.kakao.internship2019.자물쇠와열쇠;
+
+import java.util.Stack;
+
+/**
+ * 코딩테스트 연습
+ * 2020 KAKAO BLIND RECRUITMENT
+ * 자물쇠와 열쇠
+ * https://programmers.co.kr/learn/courses/30/lessons/60059
+ */
+class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean solution(int[][] key, int[][] lock) {
+
+        boolean answer = true;
+        return answer;
+    }
+}
